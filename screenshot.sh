8c17ca50ea54029db3365618d8e432708b817044
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-07-28 18:20:56 +0200

    Add a script to take screenshots

---
+++ screenshot.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S nix shell nixpkgs#playwright --command bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 output_dir" >&2
+    exit 1
+fi
+
+set -ex
+
+mkdir -p $1
+
+# Desktop
+playwright screenshot --full-page --sandbox false index.html --viewport 1920x1080 $1/1920x1080.png
+
+# iPhone SE 2nd Gen, horizontal
+playwright screenshot --full-page --sandbox false index.html --viewport 667x375 $1/667x375.png
+
+# iPhone 11 Pro
+playwright screenshot --full-page --sandbox false index.html --viewport 375x812 $1/375x812.png
